08b5a44816803999656d00c34dee5a6ef113a965
tshmeshtewi-arch <tshmeshtewi@gmail.com>
2026-07-10 00:39:21 +0200

    Add posterUrl to Movie constructor

--- app/src/main/java/com/nuxflix/app/Movie.java
+++ app/src/main/java/com/nuxflix/app/Movie.java
@@ -7,15 +7,17 @@ public class Movie {
     String duration;
     String rating;
     String videoUrl;
+    String posterUrl;
     int posterColor;
 
-    public Movie(String title, String description, String year, String duration, String rating, String videoUrl, int posterColor) {
+    public Movie(String title, String description, String year, String duration, String rating, String videoUrl, String posterUrl, int posterColor) {
         this.title = title;
         this.description = description;
         this.year = year;
         this.duration = duration;
         this.rating = rating;
         this.videoUrl = videoUrl;
+        this.posterUrl = posterUrl;
         this.posterColor = posterColor;
     }
 }
